13e135ea738aa658cac800eee4de78c7e587e6aa
zjj <805185198@qq.com>
2024-04-10 17:08:28 +0800

    完成底部导航栏根据状态跳转逻辑

--- src/components/foot_bar.js
+++ src/components/foot_bar.js
@@ -17,18 +17,41 @@ import { useNavigation } from '@react-navigation/native';
 
 import styles from '../css/foot_bar';
 
+import {connect} from 'react-redux';
+import userLogin from '../store/actions/action';
+
 function FootBar(props){
     // const {navigation}=props;
     const navigation = useNavigation();
+    // const navigation = useNavigation();
+    const {login} = props;
+
+    const clickMine = () => {
+        if(login === 'offlogin'){
+            navigation.navigate('Login');
+        }else{
+            navigation.navigate('Mine');
+        }
+    };
+    const clickAdd = () => {
+        if(login === 'offlogin'){
+            navigation.navigate('Login');
+        }else{
+            navigation.navigate('PublishTravel');
+        }
+    };
+    const clickIndex = () => {
+        navigation.navigate('AppIndex');
+    }
     return (
         <View style={styles.foot_container}>
-            <Pressable>
+            <Pressable onPress={clickIndex}>
                 <Text style={styles.foot_index}>首页</Text>
             </Pressable>
-            <Pressable onPress={()=>navigation.navigate('PublishTravel')}>
+            <Pressable onPress={clickAdd}>
                 <Text style={styles.foot_add}>+</Text>
             </Pressable>
-            <Pressable>
+            <Pressable onPress={clickMine}>
                 <Text style={styles.foot_mine}>我的</Text>
             </Pressable>
         </View>
@@ -36,4 +59,20 @@ function FootBar(props){
     );
 };
 
-export default FootBar;
+// 将状态存入props中
+const mapStateToProps = (state) => {
+    return{
+      login: state.login,
+      name: state.name,
+    }
+  };
+  // 将dispatch存入props中
+  const mapDispatchToProps = (dispatch) => {
+    return{
+        UserLogin: (manager) => dispatch(userLogin(
+          {login: manager.login, name: manager.name}
+        ))
+    }
+  };
+
+export default connect(mapStateToProps, mapDispatchToProps)(FootBar);
