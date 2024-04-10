4cab82318b9fe6441a47d6f5a6214c174903f5a7
zjj <805185198@qq.com>
2024-04-10 17:27:11 +0800

    头部导航栏

---
+++ src/components/search_header.js
@@ -0,0 +1,25 @@
+import React,{  useState } from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    Pressable,
+    Alert,
+} from 'react-native';
+import styles from '../css/search_header copy';
+
+function SearchHeader(props){
+    const [searchName, setSearchName] = useState('');
+
+    return (
+        <View style={styles.header}>
+            <TextInput style={styles.search_input} value={searchName} onChangeText={text=>setSearchName(text)} />
+            <Pressable style={styles.search_button} onPress={()=>{Alert.alert('搜索游记', searchName)}}>
+                <Text>搜索</Text>
+            </Pressable>
+        </View>
+        
+    );
+};
+
+export default SearchHeader;
