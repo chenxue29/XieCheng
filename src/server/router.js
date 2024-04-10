a710155effb69e057e7467750d74ce02d8ae47a4
Chen <2539962310@qq.com>
2024-04-10 17:33:50 +0800

    publish(除图片)

--- src/server/router.js
+++ src/server/router.js
@@ -1,7 +1,9 @@
 let express = require('express')
 let router = express.Router()
 let login = require('./API/login')
+let publish = require('./API/publish')
 
 router.post('/login',login.login)
+router.get('/publish',publish.publish)
 
 module.exports = router
